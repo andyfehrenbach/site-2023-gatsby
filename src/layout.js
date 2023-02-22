9e95039e299791e495767cee8ebc90a833a4ba5d
Andy Fehrenbach <andy.fehrenbach@gmail.com>
2023-02-22 11:43:14 -0600

    more portfolio page formatting

--- src/layout.js
+++ src/layout.js
@@ -5,6 +5,8 @@ import Head from './head';
 import '../src/styles/index.scss';
 
 export default function Layout({ children, title = false, description = false, path = false }) {
+  const colors = [`colors-1`, `colors-2`, `colors-3`, `colors-4`];
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
   return (
     <>
       <Head title={title} description={description} path={path} />
@@ -12,7 +14,7 @@ export default function Layout({ children, title = false, description = false, p
         onLoad={setTimeout(function () {
           window.scrollTo(0, 0);
         }, 200)}
-        className='u-bg--primary colors-2'
+        className={`u-bg--primary' ${randomColor}`}
       >
         <div> {children} </div>
       </div>
--- src/pages/portfolio/{mdx.frontmatter__slug}.js
+++ src/pages/portfolio/{mdx.frontmatter__slug}.js
@@ -1,40 +1,71 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 import Layout from '../../layout';
 import Logo from '../../logo';
 
 export default function ProjectLayout({ children, pageContext }) {
-  const { title, description, client, year, workingArrangement } = pageContext.frontmatter;
+  const { title, description, client, year, slug, workingArrangement } = pageContext.frontmatter;
+
+  const data = useStaticQuery(graphql`
+    query LinkQuery {
+      allMdx {
+        nodes {
+          id
+          frontmatter {
+            slug
+            title
+          }
+        }
+      }
+    }
+  `);
+
+  const links = data.allMdx.nodes;
+
   return (
     <Layout title={title} description={description}>
       <div className='portfolio-page'>
+        <h1 className='project-title-large'>{title}</h1>
         <header className='portfolio-page-header'>
           <div className='portfolio-page-header-wrapper'>
             <div className='logo-wrapper ml4'>
               <Logo className={`u-color-black ml1`}></Logo>
             </div>
-            <h2 className='project-title-small'>{title}/</h2>
-            <div className='project-detail'>
-              <h3 className='project-details-label'>Timeframe</h3>
-              <p className='project-details-data'>{year}</p>
-            </div>
-            <div className='project-detail'>
-              <h3 className='project-details-label'>Client</h3>
-              <p className='project-details-data'>{client}</p>
-            </div>
-            <div className='project-detail'>
-              <h3 className='project-details-label'>Role</h3>
-              <p className='project-details-data'>{workingArrangement}</p>
+            <div className='flex project-detail-wrapper pt1'>
+              <div className='project-detail'>
+                <h2 className='project-title-small'>{title}/</h2>
+              </div>
+              <div className='project-detail'>
+                <h3 className='project-details-label'>Timeframe</h3>
+                <p className='project-details-data'>{year}</p>
+              </div>
+              <div className='project-detail'>
+                <h3 className='project-details-label'>Client</h3>
+                <p className='project-details-data'>{client}</p>
+              </div>
+              <div className='project-detail'>
+                <h3 className='project-details-label'>Role</h3>
+                <p className='project-details-data'>{workingArrangement}</p>
+              </div>
             </div>
           </div>
         </header>
         <main className='portfolio-page-main'>
-          <div className='relative'>
-            <h1 className='project-title-large'>{title}</h1>
-          </div>
+          <div className='relative'></div>
           <section className='Content'>{children}</section>
         </main>
-        <Link to='/'>&larr; Back</Link>
+        <nav>
+          <h2 className='work'>More Work:</h2>
+          <ul>
+            {links.map((link) => (
+              <li key={link.id}>
+                <Link to={`/portfolio/${link.frontmatter.slug}`}>{link.frontmatter.title}</Link>
+              </li>
+            ))}
+          </ul>
+
+          <Link to='/'>&larr; Back</Link>
+        </nav>
       </div>
       {/* <pre>{JSON.stringify(props, null, 2)}</pre> */}
     </Layout>
